eefb9c261145bf6260bb7b37e09431d398e78982
toppy777 <toppy01257@yahoo.co.jp>
2020-02-20 03:01:05 +0000

    SQL文を実行する関数を作成

--- main.py
+++ main.py
@@ -19,10 +19,7 @@ def top():
 # 記事作成ページ
 @app.get('/create')
 def create():
-    c= ConnectDB()
-    c[1].execute("SELECT * FROM categories")
-    categories = c[1].fetchall()
-    c[1].close()
+    categories = ExecuteGetContents('SELECT * FROM categories')
     return template('views/create.html', categories=categories)
 
 # 記事のタイトル・本文が、POST送信されるとDBに挿入する
@@ -31,21 +28,16 @@ def do_create():
     title = request.POST.getunicode('title')
     content = request.POST.getunicode('content')
     category_id = request.POST.getunicode('category')
-    c = ConnectDB()
-    c[1] = conn.cursor()
-    c[1].execute("INSERT INTO blogs (title, content, created_at, category_id) \
-        VALUES( ?, ?, ?, ?)", (title, content, str(datetime.datetime.now()), category_id))
-    c[0].commit()
-    c[1].close()
+
+    ExecuteQuery('INSERT INTO blogs (title, content, created_at, category_id) VALUES( "%s", "%s", "%s", %s)' \
+                  % (title, content, str(datetime.datetime.now()), category_id))
     return template('views/top.html')
 
 # 記事一覧の閲覧
 @app.get('/showlist')
 def show_list():
-    c = ConnectDB()
-    c[1].execute("SELECT * FROM blogs INNER JOIN categories ON blogs.category_id = categories.category_id")
-    blogs = c[1].fetchall()
-    c[1].close()
+    blogs = ExecuteGetContents(\
+        'SELECT * FROM blogs INNER JOIN categories ON blogs.category_id = categories.category_id')
     if not blogs:
         return '記事はまだありません。'
     else:
@@ -54,10 +46,7 @@ def show_list():
 # 指定された記事の閲覧
 @app.get('/showblog<id:re:[0-9]+>')
 def show(id):
-    c = ConnectDB()
-    c[1].execute("SELECT * FROM blogs WHERE id = ?",(id, ))
-    blog = c[1].fetchone()
-    c[1].close()
+    blog = ExecuteGetContent('SELECT * FROM blogs WHERE id = %s' % id)
     if not blog:
         return 'このアイテムはみつかりませんでした。'
     else:
@@ -65,21 +54,14 @@ def show(id):
 
 @app.post('/deleteblog<id:re:[0-9]+>')
 def delete(id):
-    c = ConnectDB()
-    c[1].execute("DELETE FROM blogs where id = ?", (id, ))
-    c[0].commit()
-    c[1].execute("SELECT * FROM blogs")
-    blogs = c[1].fetchall()
-    c[1].close()
+    ExecuteQuery('DELETE FROM blogs where id = %s' % id)
+    blogs = ExecuteGetContents('SELECT * FROM blogs')
     return template('views/showlist.html', blogs = blogs)
 
 # 編集画面に遷移
 @app.get('/updateblog<id:re:[0-9]+>')
 def update(id):
-    c = ConnectDB()
-    c[1].execute('SELECT * FROM blogs where id = ?', (id, ))
-    blog = c[1].fetchone()
-    c[1].close()
+    blog = ExecuteGetContent('SELECT * FROM blogs WHERE id = %s' % id)
     return template('views/update.html', blog = blog)
 
 # 編集を実行
@@ -87,12 +69,8 @@ def update(id):
 def do_update(id):
     title = request.POST.getunicode('title')
     content = request.POST.getunicode('content')
-    c = ConnectDB()
-    c[1].execute("UPDATE blogs SET title = ?, content = ? where id = ?", (title, content, id))
-    c[0].commit()
-    c[1].execute('SELECT * FROM blogs where id = ?', (id, ))
-    blog = c[1].fetchone()
-    c[1].close()
+    ExecuteQuery('UPDATE blogs SET title = "%s", content = "%s" where id = %s' % (title, content, id))
+    blog = ExecuteGetContent('SELECT * FROM blogs WHERE id = %s' % id)
     return template('views/show.html', blog = blog)
 
 @app.get('/create_category')
@@ -101,16 +79,34 @@ def create_category():
 @app.post('/create_category')
 def create_category():
     category_name = request.POST.getunicode('category')
-    c = ConnectDB()
-    c[1].execute("INSERT INTO categories (category_name) values (?)", (category_name, ))
-    c[0].commit()
-    c[1].close()
+    ExecuteQuery('INSERT INTO categories (category_name) values ("%s")' % category_name)
     return template('views/top.html')
 
 def ConnectDB():
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
     return conn, c
+
+def ExecuteQuery(query):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+
+def ExecuteGetContents(get_contents_query):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(get_contents_query)
+    result = cursor.fetchall()
+    return result
+
+def ExecuteGetContent(get_content_query):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute(get_content_query)
+    result = cursor.fetchone()
+    return result
 
 run(app=app, host='localhost', port=8080, reloader=True, debug=True)
 # c.execute('CREATE TABLE BLOGS(id integer primary key autoincrement, title text, content text, date date)')
